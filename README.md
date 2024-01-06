da89daaea9b28614274320ec010da96cbb5e146d
Yell <xyellx@gmail.com>
2024-01-06 13:13:47 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# PrinterBackupScript
+Script to backup MainsailOS (and likely others) to github
+
+# Prep
+* Sign into GitHub and create a new repository
+* make a Deployment Key  https://docs.github.com/en/authentication/connecting-to-github-with-ssh/managing-deploy-keys#deploy-keys
+* Initialize git
+
+
+# Install
+Login to your machine via ssh and run these commands:
+```
+cd ~
+git clone https://github.com/Pockrandt/PrinterBackupScript.git
+chmod +x PrinterBackupScript/autocommit.sh
+```
+
+Now, add this section to your moonraker.conf via GUI or CLI (nano ~/printer_data/config/moonraker.conf)
+
+```
+[update_manager PrinterBackupScript]
+type: git_repo
+channel: dev
+path: ~/PrinterBackupScript
+origin: https://github.com/Pockrandt/PrinterBackupScript.git
+is_system_service: False
+```
+
+Note: maybe not trust a stranger on the internet.. you can however fork THIS repo and add your own.
+(edit the github path inside the install.sh)
+
+# How to backup
+
+## Manual
+ ssh into the pi and `~/PrinterBackupScript/autocommit.sh`
+
+## at a event
+
+Note: if the user has no existing crontab, you may see this message: `no crontab for <username>`
+
+(it still works, even if you see that message.)
+
+on reboot
+```
+crontab -l | { cat; echo "@reboot /home/pi/PrinterBackupScript/autocommit.sh >/dev/null 2>&1"; } | crontab -
+```
+
+every 6 hours
+```
+crontab -l | { cat; echo "* */6 * * * /home/pi/PrinterBackupScript/autocommit.sh >/dev/null 2>&1"; } | crontab -
+```
+(use this site to create the cron entry https://crontab-generator.org you can also edit the crontab interactivly via `crontab -e` 
+
+## Gcode
+install Klipper gcode extension and restart klipper
+```
+wget -P ~/klipper/klippy/extras/ https://raw.githubusercontent.com/dw-0/kiauh/master/resources/gcode_shell_command.py
+```
+
+then make a new conf and include it, or put in into another one
+```
+[gcode_shell_command backup_to_github]
+command: /usr/bin/bash /home/pi/PrinterBackupScript/autocommit.sh
+timeout: 30.0
+verbose: True
+
+[gcode_macro BACKUP_CONFIG]
+description: Backs up config directory GitHub
+gcode:
+   RUN_SHELL_COMMAND CMD=backup_to_github
+
+```
+
+
+# credits
+https://docs.vorondesign.com/community/howto/EricZimmerman/BackupConfigToGithub.html
---
+++ autocommit.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+
+### Changes
+# Sep 10, 2023 
+#  removed legacy token notes
+#  marked script with filemod +x
+#  renamed default branch
+# Jul 24, 2023 Initial Version from https://github.com/EricZimmerman/VoronTools/blob/main/autocommit.sh
+###
+
+#######################################################################
+## NOTE: This script originates from here but I tweaked the pull     ##
+## command, changed default location for backup, and added a comment ##
+## for reference later.                                              ##
+#######################################################################
+
+#####################################################################
+### Please set the paths accordingly. In case you don't have all  ###
+### the listed folders, just keep that line commented out.        ###
+#####################################################################
+### Path to your config folder you want to backup
+config_folder=~/printer_data/config/printerconfig
+
+# NOTE: The above should work for just about everyone, but a somewhat
+# recent update to moonraker changed paths, etc. You can run the 
+# provided moonraker script 'data-path-fix.sh' to fix/update
+# older installs
+
+### Path to your Klipper folder, by default that is '~/klipper'
+klipper_folder=~/klipper
+
+### Path to your Moonraker folder, by default that is '~/moonraker'
+moonraker_folder=~/moonraker
+
+### Path to your Mainsail folder, by default that is '~/mainsail'
+mainsail_folder=~/mainsail
+
+### Path to your Fluidd folder, by default that is '~/fluidd'
+#fluidd_folder=~/fluidd
+
+### The branch of the repository that you want to save your config
+### By default that is 'master', its now 'main' because you know slavery and such ..
+branch=main
+
+#####################################################################
+#####################################################################
+
+
+#####################################################################
+################ !!! DO NOT EDIT BELOW THIS LINE !!! ################
+#####################################################################
+grab_version(){
+  if [ ! -z "$klipper_folder" ]; then
+    cd "$klipper_folder"
+    klipper_commit=$(git rev-parse --short=7 HEAD)
+    m1="Klipper on commit: $klipper_commit"
+    cd ..
+  fi
+  if [ ! -z "$moonraker_folder" ]; then
+    cd "$moonraker_folder"
+    moonraker_commit=$(git rev-parse --short=7 HEAD)
+    m2="Moonraker on commit: $moonraker_commit"
+    cd ..
+  fi
+  if [ ! -z "$mainsail_folder" ]; then
+    mainsail_ver=$(head -n 1 $mainsail_folder/.version)
+    m3="Mainsail version: $mainsail_ver"
+  fi
+  if [ ! -z "$fluidd_folder" ]; then
+    fluidd_ver=$(head -n 1 $fluidd_folder/.version)
+    m4="Fluidd version: $fluidd_ver"
+  fi
+}
+
+push_config(){
+  cd $config_folder
+  git pull origin $branch --no-rebase
+  git add .
+  current_date=$(date +"%Y-%m-%d %T")
+  git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
+  git push origin $branch
+}
+
+grab_version
+push_config
